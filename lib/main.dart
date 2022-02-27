0d74e3316159b7fb7b686bb552cc3f59280e00f8
mkkrishna12 <malikrushna333@gmail.com>
2022-02-27 13:52:35 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
+import 'package:census/form.dart';
 import 'package:census/login-Page/routes/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  await Firebase.initializeApp();
-  WidgetsFlutterBinding.ensureInitialized();
+import 'multi_contact_form_widget.dart';
 
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Login(),
-    );
+      home: MultiContactFormWidget(),    );
   }
 }
